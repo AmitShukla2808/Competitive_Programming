b2ea29ff5f50f9ff52cd48d1d90c531341f44373
Amit Shukla <77337715+AmitShukla2808@users.noreply.github.com>
2021-05-22 17:42:37 +0530

    Add files via upload
    An excellent questions from SPOJ on binary search.

---
+++ aggressivecows.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int solve(vector<int>A,int dis,int co){
+    int pos=A[0],cows=1; 
+    for(int i=0; i<A.size(); i++){
+        if(A[i]-pos>=dis){
+            cows++; 
+            pos=A[i];
+        }
+    }
+    if(cows>=co){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int main(int argc, char const *argv[])
+{   int n;
+    cout<<"Enter the number of stalls"<<endl;
+    cin>>n;
+    vector<int>A(n);
+    cout<<"Enter the location of each stall in order"<<endl;
+    for(int i=0; i<n; i++){
+        cin>>A[i];
+    }
+    int cows;
+    cout<<"Enter the number of cows to be seated"<<endl;
+    cin>>cows;
+    int start=1,end=A[n-1]-A[0],mid=0,ans=0;
+    while(start<=end){
+        mid=start+(end-start)/2;
+        if(solve(A,mid,cows)){
+            ans=mid;
+            start=mid+1;
+        }
+        else{
+            end=mid-1;
+        }
+    }
+    cout<<"The largest minimum distance between cows is : "<<ans<<endl;
+    return 0;
+}
