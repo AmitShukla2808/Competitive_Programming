6cab49025b6d62a625860ff31dd6a3b8d5fdf2ae
Amit Shukla <77337715+AmitShukla2808@users.noreply.github.com>
2021-05-30 21:31:10 +0530

    Add files via upload
    Find the maximum element for each sliding of the sliding window of a given length in an array. Used multiset

---
+++ slidingwindowmaximum.cpp
@@ -0,0 +1,42 @@
+//Constraints: 1<=n<=10^6, 1<=A[i]<=10^9
+//Time Complexity: O(n)
+// Given an array and length of sliding window, find maximum element in each window5
+
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> solve(vector<int>A, int k){
+    multiset<int>B; vector<int>ans;
+    int i=0,pos=0;
+    for(i=0; i<k; i++){
+        B.insert(A[i]);
+    }
+    ans.push_back(*(--B.end()));
+    B.erase(A[pos]); pos++;
+    while(i<A.size()){
+        B.insert(A[i]);
+        ans.push_back(*(--B.end()));
+        B.erase(A[pos]); pos++; i++;
+    }
+    return ans;
+}
+int main(int argc, char const *argv[])
+{
+    int n,k;
+    cout<<"Enter the number of elements"<<endl;
+    cin>>n;
+    vector<int>A(n);
+    cout<<"Enter the elements in the vector"<<endl;
+    for(int i=0; i<n; i++){
+        cin>>A[i];
+    }
+    cout<<"Enter the length of sliding window"<<endl;
+    cin>>k;
+    vector<int>ans;
+    ans = solve(A,k);
+    cout<<"The maximum values in each window are : ";
+    for(int i=0; i<ans.size(); i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
