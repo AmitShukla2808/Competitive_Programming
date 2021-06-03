4726b89c78aefe27364c1d7970dcf2f04dae92ed
Amit Shukla <amushuk890@gmail.com>
2021-06-03 10:44:35 +0530

    Excellent question on bit manipulation

---
+++ singlepresencektimes.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int solve(vector<int>A,int k){
+    vector<int>bits(32,0); int ans=0;
+    for(int i=0; i<bits.size(); i++){
+        for(int j=0; j<A.size(); j++){
+            if(A[j]&(1<<i)){
+                bits[i]++;
+            }
+        }
+    }
+    for(int i=0;i<bits.size();i++){
+        ans+=(bits[i]%k)*(1<<i);
+    }
+    return ans;
+}
+int main(int argc, char const *argv[])
+{
+    int n;
+    cout<<"Enter the number of elements"<<endl;
+    cin>>n;
+    int k;
+    cout<<"Enter how many number of times will each element be repeated except one"<<endl;
+    cin>>k;
+    vector<int>A(n);
+    cout<<"Enter the elements in the vector"<<endl;
+    for(int i=0; i<n; i++){
+        cin>>A[i];
+    }
+    int ans = solve(A,k);
+    cout<<"The number repeated once is : "<<ans<<endl;
+    return 0;
+}
