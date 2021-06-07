1c5ace4d0ead7c80827855d73fd877739de8bcf5
Amit Shukla <amushuk890@gmail.com>
2021-06-07 10:29:45 +0530

    No changes made

--- binaryexponentiation.cpp
+++ binaryexponentiation.cpp
@@ -1,12 +1,12 @@
 //Constraints: 1<=t<=10^5, 1<=n,m<=10^6
 //Time Complexity: O(t*log(max(n,m))) = O(nlog(n))
-// The question has been approached by binary exponentiation technique to calculate higher powers 
+// The question has been approached by binary exponentiation technique to calculate higher powers
 // without overflow.
 // Binary Exponentiation: if we want to calculate N^M , we can calculate this in O(log(m)) time by
 // seeing the set bits in M. Suppose we want to calculate 3^100 which will cause overflow and TLE. We
 // can do this as binary equivalent of 100 is 64 + 32 + 4 = 1100100 so we can write 3^100 as
 // (3^64)*(3^32)*(3^4) so this can be done by shifting bits of 100 to right one by one and check if the
-// bit at 1 is set then we multiply the the power to ans else not. We have reduced time to log(m) from 
+// bit at 1 is set then we multiply the the power to ans else not. We have reduced time to log(m) from
 // m. Furthermore we can continuously mod to check overflow.
 
 #include <bits/stdc++.h>
